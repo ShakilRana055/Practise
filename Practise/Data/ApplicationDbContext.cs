e0d62e907813063fbdda7829c9fdd0503c0279b5
Shakil Rana <36135717+ShakilRana055@users.noreply.github.com>
2021-03-02 19:59:59 +0600

    Add project files.

---
+++ Practise/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practise.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> option ): base(option)
+        {
+
+        }
+
+        //public DbSet<SomeClass> SomeClasses { get; set; }
+    }
+}
